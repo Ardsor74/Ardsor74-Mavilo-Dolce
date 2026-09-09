39ae54f4bfa7cfea845fb830d9b340ca9b211b96
Ardsor74 <sorrentinoarnaldo@gmail.com>
2026-09-10 00:07:57 +0200

    Aggiunta pagina anteprima alla whitelist build

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("uploads");
   eleventyConfig.addPassthroughCopy("atelier.jpg");
   eleventyConfig.addPassthroughCopy("logo-mavilo-dolce.png");
+  eleventyConfig.addPassthroughCopy("preview-nuovo-sito.html");
   eleventyConfig.addPassthroughCopy("robots.txt");
 
   // Video (solo quelli effettivamente presenti)
